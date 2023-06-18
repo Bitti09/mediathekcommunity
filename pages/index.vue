77f587767f14ef88985fc15b2beda665c1d6d51b
Bitti <Bitti09@users.noreply.github.com>
2023-06-19 00:26:45 +0200

    add geoloc server part 1/x
    Signed-off-by: Bitti <Bitti09@users.noreply.github.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <template>
     <v-container fluid>
-         <v-row>
-        <v-col v-for="post in posts" :key="post.id" dense>
-                 <Cardhome :posts="post"></Cardhome>
-         </v-col>
-    </v-row>
+        Geo : {{ info }}
+        <v-row>
+            <v-col v-for="post in posts" :key="post.id" dense>
+                <Cardhome :posts="post"></Cardhome>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 <script setup>
@@ -12,6 +13,7 @@ import { onMounted } from 'vue'
 const router = useRouter();
 const { getItems } = useDirectusItems();
 const posts = await getItems({ collection: "mediathek", params: { fields: ['*,coverimage.id,heroimage.id'] } });
+const { data: info } = await useFetch('/api/info')
 
 </script>
 <script>
