922ab07813b35e68e6ac5491ba183f7528a3f49a
Bitti <Bitti09@users.noreply.github.com>
2023-06-20 00:40:09 +0200

    more work on carddetails

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,30 +3,40 @@ export default defineNuxtConfig({
     devtools: {
         enabled: false
     },
-    css: ['vuetify/lib/styles/main.sass',
+
+    css: ['vuetify/lib/styles/main.sass', '@vime/core/themes/default.css'
+
     ],
+
     modules: ['nuxt-directus', 'nuxt-cloudflare-analytics', 'nuxt-icon', 'nuxt-lodash', 'nuxt-swiper'
     ],
+
     build: {
         transpile: ['vuetify'],
     },
+
     vite: {
         define: {
             'process.env.DEBUG': false,
         },
     },
+
     directus: {
         autoFetch: true,
         url: 'https://api.mediathek.community/',
         token: 'Oxq-DfaVvPcWI4tNjwlMNeHUZT0q_SZE',
     },
+
     cloudflareAnalytics: {
         // See below for more options
         token: 'f0c80b668b0e43239ca441d5ea90cab5', // Example 1a2b3v4a5er6ac7r8afd
     },
+
     vue: {
         compilerOptions: {
             isCustomElement: tag => ['swiper-container', 'swiper-slide'].includes(tag),
         },
     },
+
+    plugins: ['/c:/Users/bitti/Documents/mediathek.-nuxt/nuxt-app/plugins/vuetify.js'],
 });
