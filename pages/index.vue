175ae9dd25ad850015b9946cd64138bc2c16e65f
Bitti <Bitti09@users.noreply.github.com>
2023-06-19 21:32:41 +0200

    .
    Signed-off-by: Bitti <Bitti09@users.noreply.github.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,20 @@
 <template>
     <v-container fluid>
         <v-card title="Zuletzt hinzugefügt" variant="outlined">
-            <swiper-container :slides-per-view="4" :loop="true">
+            <swiper-container slides-per-view="2" space-between="0" loop="false" breakpoints="{
+                640: {
+                    slidesPerView: 2,
+                    spaceBetween: 0,
+                },
+                768: {
+                    slidesPerView: 4,
+                    spaceBetween: 0,
+                },
+                1024: {
+                    slidesPerView: 5,
+                    spaceBetween: 0,
+                }
+            }">
                 <template v-for="(post, i) in posts" :key="post.id">
                     <swiper-slide>
                         <Cardhome :posts="post" :showtype="true"></Cardhome>
@@ -10,7 +23,20 @@
             </swiper-container>
         </v-card>
         <v-card title="Deutsche Mediatheken" variant="outlined" v-show="grouped.includes('de')">
-            <swiper-container slides-per-view="4" loop="true">
+            <swiper-container :slides-per-view="auto" :space-between="spaceBetween" :centered-slides="false" :breakpoints="{
+                640: {
+                    slidesPerView: 2,
+                    spaceBetween: 0,
+                },
+                768: {
+                    slidesPerView: 4,
+                    spaceBetween: 0,
+                },
+                1024: {
+                    slidesPerView: 5,
+                    spaceBetween: 0,
+                }
+            }" :loop="false">
                 <template v-for="(post, i) in posts" :key="post.id">
                     <swiper-slide v-if="post.country == 'de'">
                         <Cardhome :posts="post" :showtype="true"></Cardhome>
@@ -19,7 +45,20 @@
             </swiper-container>
         </v-card>
         <v-card title="Schwedische Mediatheken" variant="outlined" v-show="grouped.includes('sw')">
-            <swiper-container slides-per-view="4" loop="true">
+            <swiper-container :slides-per-view="2" :breakpoints="{
+                640: {
+                    slidesPerView: 2,
+                    spaceBetween: 0,
+                },
+                768: {
+                    slidesPerView: 4,
+                    spaceBetween: 0,
+                },
+                1024: {
+                    slidesPerView: 5,
+                    spaceBetween: 0,
+                }
+            }" :loop="false">
                 <template v-for="(post, i) in posts" :key="post.id">
                     <swiper-slide v-if="post.country == 'sw'">
                         <Cardhome :posts="post" :showtype="true"></Cardhome>
@@ -41,9 +80,10 @@ posts.value = await getItems({ collection: "mediathek", params: { fields: ['*,co
 // import function to register Swiper custom elements
 // register Swiper custom elements
 register();
-watch(posts, (newValue, oldValue) => {
+ watch(posts, (newValue, oldValue) => {
     if (newValue.length != 0) {
         grouped.value = keys(countBy(newValue, function (newValue) { return newValue.country; }))
+        refresh()
     }
 }, { immediate: true })
 </script>
