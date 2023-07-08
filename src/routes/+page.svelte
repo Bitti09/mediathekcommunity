777f5a8bbe216ff56f74843d5b94e4c0a0bf8237
Bitti <Bitti09@users.noreply.github.com>
2023-07-08 16:24:09 +0200

    init new svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let data;
+</script>
+
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		{data.article[0].title}
+	</div>
+</div>
