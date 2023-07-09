2ea945c77687f569ccdb6a134d4e581da74703b3
Bitti <Bitti09@users.noreply.github.com>
2023-07-09 15:26:24 +0200

    more update to modal

--- src/lib/components/ModalComponentOne.svelte
+++ src/lib/components/ModalComponentOne.svelte
@@ -39,7 +39,7 @@
 	}
 	function playepisode(quality, alldata) {
 		showvideo = true;
-		tabSet = 0;
+		//tabSet = 0;
 		switch (quality) {
 			case 'hd':
 				modalvideo.set({
@@ -75,7 +75,7 @@
 </script>
 
 {#if $modalStore[0]}
-	<div class="modal-example-form {cBase}">
+	<div class="modal-example-form {cBase} test1">
 		<div class="card">
 			<header class="card-header p-0">
 				<div class="h-1/4">
@@ -98,7 +98,7 @@
 				</div>
 			</header>
 			<section class="p-0 card-t px-0 w-full">
-				<TabGroup justify="justify-center">
+				<TabGroup justify="justify-left">
 					<Tab bind:group={tabSet} name="tab1" value={0}>
 						<span>Infos</span>
 					</Tab>
@@ -106,49 +106,61 @@
 						<Tab bind:group={tabSet} name="tab2" value={1}>Links</Tab>
 					{/if}
 					{#if $modalProps.category == 'series'}
-						<Tab bind:group={tabSet} name="tab3" value={2}>Episoden - OmU</Tab>
-						<Tab bind:group={tabSet} name="tab4" value={3}>Episoden</Tab>
+						<Tab bind:group={tabSet} name="tab3" value={2}>Links(OmU)</Tab>
+						<Tab bind:group={tabSet} name="tab4" value={3}>Links</Tab>
 					{/if}
 					<!-- Tab Panels --->
 					<svelte:fragment slot="panel">
 						{#if tabSet === 0}
-							<div class="table-container p-0 mt-0">
-								<!-- Native Table Element -->
-								<table class="table table-hover">
-									<tbody>
-										<tr>
-											<td colspan="3">Sender</td>
-											<td>{$modalProps.channel} </td>
-										</tr>
-										{#if ($modalProps.category == 'series')}
-											<tr>
-												<td colspan="3">Staffeln</td>
-												<td>{$modalProps.episodes} </td>
-											</tr>
-                                            <tr>
-												<td colspan="3">Folgen</td>
-												<td>{$modalProps.listepisodes.length} </td>
-											</tr>
-										{/if}
-										<tr>
-											<td colspan="3">Quality</td>
-											<td>{$modalProps.quality} </td>
-										</tr>
-										<tr>
-											<td colspan="3">publishdate </td>
-											<td>{$modalProps.publishdate} </td>
-										</tr>
-										<tr>
-											<td colspan="3">Online until</td>
-											<td>{$modalProps.delistdate} </td>
-										</tr>
-										<tr>
-											<td colspan="3">description</td>
-											<td>{$modalProps.description} </td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
+						
+<div class=" shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Sender
+                </th>
+                <td class="px-6 py-4">
+                    {$modalProps.channel}
+                </td>
+            </tr>
+			{#if ($modalProps.category == 'series')}
+            <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Staffeln
+                </th>
+                <td class="px-6 py-4">
+                    {$modalProps.episodes}
+                </td>
+            </tr>		
+            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Folgen
+                </th>
+                <td class="px-6 py-4">
+                    {$modalProps.listepisodes.length}
+                </td>
+            </tr>
+			{/if}
+            <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Quality
+                </th>
+                <td class="px-6 py-4">
+                    {$modalProps.quality}
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    description
+                </th>
+                <td class="px-6 py-4">
+                    {$modalProps.description}
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 						{:else if tabSet === 1}
 							<button type="button" class="btn variant-filled mt-0" on:click={playvideo}
 								>play</button
@@ -253,4 +265,13 @@
 	.dark .card-t {
 		background-color: rgb(var(--color-surface-800));
 	}
+	.test1 {
+width: 90%;				
+    height:90%;
+    margin:0 auto;
+	}
+	.test {
+    overflow-wrap: break-all !important;
+    word-wrap: break-all !important;  /* IE */
+}
 </style>
