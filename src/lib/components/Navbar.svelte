0179546abd1457c85d5592f0889c041a4514a289
Bitti <Bitti09@users.noreply.github.com>
2023-07-09 02:27:37 +0200

    push

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -8,16 +8,20 @@
 		<AppRailAnchor href="/">(icon)</AppRailAnchor>
 	</svelte:fragment>
 	<!-- --- -->
-	<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
-		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+    <AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+		<svelte:fragment slot="lead"><i class="fa-solid fa-house"/></svelte:fragment>
 		<span>Tile 1</span>
 	</AppRailTile>
-	<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
-		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+	<AppRailTile bind:group={currentTile} name="tile-1.1" value={1} title="tile-11">
+		<svelte:fragment slot="lead"><i class="fa-solid fa-video"/></svelte:fragment>
+		<span>Tile 1</span>
+	</AppRailTile>
+	<AppRailTile bind:group={currentTile} name="tile-2" value={2} title="tile-2">
+		<svelte:fragment slot="lead"><i class="fa-solid fa-tv"/></svelte:fragment>
 		<span>Tile 2</span>
 	</AppRailTile>
-	<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
-		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+	<AppRailTile bind:group={currentTile} name="tile-3" value={3} title="tile-3">
+		<svelte:fragment slot="lead"><i class="fa-solid fa-music"/></svelte:fragment>
 		<span>Tile 3</span>
 	</AppRailTile>
 	<!-- --- -->
