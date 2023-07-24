442037b9f829e8200880a11baff1acc85aa79a48
Bitti <Bitti09@users.noreply.github.com>
2023-07-25 00:33:04 +0200

    init fresh2

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,114 @@
+<script>
+	// @ts-nocheck
+	import {
+		VideoSolid,
+		HouseChimneySolid,
+		TvSolid,
+		MusicSolid,
+		MasksTheaterSolid,
+		ClockSolid
+	} from 'svelte-awesome-icons';
+
+	import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
+	import { drawerStore } from '@skeletonlabs/skeleton';
+	function onClickAnchor() {
+		drawerStore.close();
+	}
+	let currentTile;
+</script>
+
+<AppRail>
+	<!-- --- -->
+	<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+		<AppRailAnchor
+			data-sveltekit-reload
+			href="/"
+			selected={$page.url.pathname === '/'}
+			on:click={() => {
+				onClickAnchor();
+			}}
+		>
+			<svelte:fragment slot="lead"><HouseChimneySolid /></svelte:fragment>
+			<span>Home</span>
+		</AppRailAnchor>
+	</AppRailTile>
+	<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+		<AppRailAnchor
+			data-sveltekit-reload
+			href="/movie"
+			selected={$page.url.pathname === '/movie'}
+			on:click={() => {
+				onClickAnchor();
+			}}
+		>
+			<svelte:fragment slot="lead"><VideoSolid /></svelte:fragment>
+			<span>Movies</span>
+		</AppRailAnchor>
+	</AppRailTile>
+	<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
+		<AppRailAnchor
+			data-sveltekit-reload
+			href="/series"
+			selected={$page.url.pathname === '/series'}
+			on:click={() => {
+				onClickAnchor();
+			}}
+		>
+			<svelte:fragment slot="lead"><TvSolid /></svelte:fragment>
+			<span>Serien</span>
+		</AppRailAnchor>
+	</AppRailTile>
+	<AppRailTile bind:group={currentTile} name="tile-3" value={3} title="tile-3">
+		<AppRailAnchor
+			data-sveltekit-reload
+			href="/music"
+			selected={$page.url.pathname === '/music'}
+			on:click={() => {
+				onClickAnchor();
+			}}
+		>
+			<svelte:fragment slot="lead"><MusicSolid /></svelte:fragment>
+			<span>Musik</span>
+		</AppRailAnchor>
+	</AppRailTile>
+	<AppRailTile bind:group={currentTile} name="tile-4" value={4} title="tile-4">
+		<AppRailAnchor
+			data-sveltekit-reload
+			href="/culture"
+			selected={$page.url.pathname === '/culture'}
+			on:click={() => {
+				onClickAnchor();
+			}}
+		>
+			<svelte:fragment slot="lead"><MasksTheaterSolid /></svelte:fragment>
+			<span>Kultur</span>
+		</AppRailAnchor>
+	</AppRailTile>
+	<AppRailTile bind:group={currentTile} name="tile-5" value={5} title="tile-5">
+		<AppRailAnchor
+			data-sveltekit-reload
+			href="/specials"
+			selected={$page.url.pathname === '/specials'}
+			on:click={() => {
+				onClickAnchor();
+			}}
+		>
+			<svelte:fragment slot="lead"><ClockSolid /></svelte:fragment>
+			<span>Specials</span>
+		</AppRailAnchor>
+	</AppRailTile>
+	<AppRailTile bind:group={currentTile} name="tile-6" value={6} title="tile-6">
+		<AppRailAnchor
+			data-sveltekit-reload
+			href="/others"
+			selected={$page.url.pathname === '/others'}
+			on:click={() => {
+				onClickAnchor();
+			}}
+		>
+			<svelte:fragment slot="lead"><MusicSolid /></svelte:fragment>
+			<span>Sonstige</span>
+		</AppRailAnchor>
+	</AppRailTile>
+</AppRail>
---
+++ src/routes/[[id]]/+page.svelte
@@ -0,0 +1,102 @@
+<script>
+	// @ts-nocheck
+	// Modals Utils
+	import { onMount } from 'svelte';
+	// import function to register Swiper custom elements
+	import { register } from 'swiper/element/bundle';
+	import { groupBy } from 'lodash-es';
+	import Card from '$lib/components/Card.svelte';
+
+	// register Swiper custom elements
+	register();
+	export let data;
+	let d1;
+	let keyz;
+	let grouped;
+	d1 = data.article;
+	if (data.param == 'specials') {
+		grouped = groupBy(d1, (item) => item.specials.Name);
+		keyz = Object.keys(grouped);
+	} else {
+		grouped = groupBy(d1, (item) => item.country);
+		keyz = Object.keys(grouped);
+	}
+	onMount(async () => {
+		const swiperParams = {
+			breakpoints: {
+				0: {
+					slidesPerView: 1,
+					spaceBetween: 1
+				},
+				490: {
+					slidesPerView: 2,
+					spaceBetween: 1
+				},
+				960: {
+					slidesPerView: 3,
+					spaceBetween: 1
+				},
+				1280: {
+					slidesPerView: 4,
+					spaceBetween: 1
+				},
+				1900: {
+					slidesPerView: 5,
+					spaceBetween: 1
+				},
+				2300: {
+					slidesPerView: 6,
+					spaceBetween: 1
+				}
+			},
+			on: {
+				init() {
+					// ...
+				}
+			}
+		};
+
+		const swiperEl = document.querySelectorAll('swiper-container');
+		for (let i = 0; i < swiperEl.length; i++) {
+			Object.assign(swiperEl[i], swiperParams);
+			swiperEl[i].initialize();
+		}
+	});
+</script>
+
+{#if data.article.length > 0}
+	<div>
+		<h1 class="h1 pb-3">
+			<span
+				class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
+				>Zuletzt hinzugefügt.</span
+			>
+		</h1>
+		<swiper-container init="false" class="'mySwiper2">
+			{#each data.article as name}
+				<swiper-slide><Card carddata={name} /></swiper-slide>
+			{/each}
+		</swiper-container>
+		{#each keyz as lang}
+			<h1 class="h1 pb-3">
+				<span
+					class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
+					>{lang}</span
+				>
+			</h1>
+			<swiper-container init="false" class="mySwiper2">
+				{#each grouped[lang] as name}
+					<swiper-slide><Card carddata={name} /></swiper-slide>
+				{/each}
+			</swiper-container>
+		{/each}
+	</div>
+{:else}
+	<h1 class="h1">
+		<span
+			class="bg-gradient-to-br from-pink-100 to-red-900 bg-clip-text text-transparent box-decoration-clone"
+			>No Item(s) found for category:</span
+		>
+		{data.param}
+	</h1>
+{/if}
