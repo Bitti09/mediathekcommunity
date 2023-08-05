7d56309793eb077172bcf112fd69ddecdc41e8be
Bitti <Bitti09@users.noreply.github.com>
2023-08-06 01:25:28 +0200

    more work in convert  svelte to astro

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/flowbite/**/*.js'
-	],
-	theme: {
-		extend: {},
-	},
-	darkMode: 'class',
-	plugins: [
-		require('flowbite/plugin')
-	],
-}
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [require("flowbite/plugin")],
+};
