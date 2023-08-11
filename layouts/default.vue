997e3289cf161397cd71def3775f5eb9578732dc
Bitti <Bitti09@users.noreply.github.com>
2023-08-11 19:23:51 +0200

    Update default.vue
    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,23 @@
 <template>
     <VApp :theme="isDark ? 'dark' : 'light'">
-
-  <VLayout class="rounded rounded-md">
-    <AppBar />
-    <AppDrawer />
-    <VMain>
-        is dark? {{ isDark }}
-        {{ t('xxx') }}
-        <slot />
-    </VMain>
-    <VFooter app class="d-flex flex-column">
-        <span class="flex-auto text-gray-500 dark:text-gray-400 text-xs md:text-lg">
-            This project uses the TMDB API but is not endorsed or certified by TMDB. - Data & Images
-            provided by TMDB
-        </span>
-    </VFooter>
-    </VLayout>
-</VApp>
-
+        <VLayout class="rounded rounded-md">
+            <AppBar />
+            <AppDrawer />
+            <VMain>
+                is dark? {{ isDark }}
+                {{ t('xxx') }}
+                <slot />
+            </VMain>
+            <VFooter app class="d-flex flex-column">
+                <span class="flex-auto text-gray-500 dark:text-gray-400 text-xs md:text-lg">
+                    This project uses the TMDB API but is not endorsed or certified by TMDB. - Data & Images
+                    provided by TMDB
+                </span>
+            </VFooter>
+        </VLayout>
+    </VApp>
 </template>
 <script setup>
-const value = reactive({
-    name1: undefined,
-    name2: undefined,
-    name3: undefined,
-})
-const { locale, locales, t, setLocale } = useI18n()
- 
- import { useAppStore } from '@/stores/app'
 import { useCustomTheme } from '~/composables/useCustomTheme'
-const app = useAppStore()
-const { isDark, toggle } = useCustomTheme()
+const { isDark } = useCustomTheme()
 </script>
