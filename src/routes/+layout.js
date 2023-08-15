4d3323e6eeb079b4fb1ebcaf8c25ee4e2b923c62
Bitti <Bitti09@users.noreply.github.com>
2023-08-16 00:09:22 +0200

    test vercel

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -2,3 +2,7 @@
 It is needed in order to circumvent a current limitation of SvelteKit. See https://github.com/inlang/inlang/issues/647
 You can remove this comment and modify the file as you like. We just need to make sure it exists.
 Please do not delete it (inlang will recreate it if needed). */
+/** @type {import('@sveltejs/adapter-vercel').Config} */
+export const config = {
+	runtime: 'edge'
+};
