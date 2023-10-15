198c0d9ce541882b3b7e6ace30c801d20c0afb6b
Bitti <Bitti09@users.noreply.github.com>
2023-10-15 14:59:58 +0200

    chore: delete +layout.js

--- src/routes/+layout.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('@sveltejs/adapter-vercel').Config} */
-export const config = {
-	runtime: 'edge'
-};
-import { dev } from '$app/environment';
-import { inject } from '@vercel/analytics';
- 
-inject({ mode: dev ? 'development' : 'production' });
