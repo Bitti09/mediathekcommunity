130ba515e1c202867f2e355faee63e1772be2ac8
Bitti <Bitti09@users.noreply.github.com>
2024-03-01 20:06:51 +0100

    add  directus sdk
    Signed-off-by: Bitti <Bitti09@users.noreply.github.com>

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLoadingIndicator />
+
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+
+    <UNotifications />
+    <UModals />
   </div>
 </template>
+
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const color = computed(() => colorMode.value === 'dark' ? '#111827' : 'white')
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+const route = useRoute()
+const appConfig = useAppConfig()
+const defaultColors = ref(['green', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet'].map(color => ({ label: color, chip: color, click: () => appConfig.ui.primary = color })))
+const colors = computed(() => defaultColors.value.map(color => ({ ...color, active: appConfig.ui.primary === color.label })))
+
+const footerLinks = [{
+    label: 'Invite people',
+    icon: 'i-heroicons-plus',
+    to: '/settings/members'
+}, {
+    label: 'Help & Support',
+    icon: 'i-heroicons-question-mark-circle',
+}]
+const links = [{
+    id: 'home',
+    label: 'Home',
+    icon: 'i-heroicons-home',
+    to: '/',
+    tooltip: {
+        text: 'Home',
+        shortcuts: ['G', 'H']
+    }
+}]
+</script>
+<template>
+    <UDashboardLayout>
+        <UDashboardPanel :width="250" :resizable="{ min: 200, max: 300 }" collapsible>
+            <UDashboardNavbar class="!border-transparent" :ui="{ left: 'flex-1' }">
+                <template #left>
+                    Mediathek Community
+                </template>
+            </UDashboardNavbar>
+
+            <UDashboardSidebar>
+                <template #header>
+                    <!--   <UDashboardSearchButton /> -->
+                </template>
+
+                <UDashboardSidebarLinks :links="links" />
+
+                <!--  <UDashboardSidebarLinks :links="[{ label: 'Colors', draggable: true, children: colors }]"
+                    @update:links="colors => defaultColors = colors" />
+ -->
+                <div class="flex-1" />
+
+
+
+                <template #footer>
+                    <!-- ~/components/UserDropdown.vue  -->
+                    <UserDropdown />
+                </template>
+            </UDashboardSidebar>
+        </UDashboardPanel>
+
+        <slot />
+
+        <!-- ~/components/HelpSlideover.vue 
+      <HelpSlideover />
+        ~/components/NotificationsSlideover.vue  
+      <NotificationsSlideover />
+  
+      <ClientOnly>
+        <LazyUDashboardSearch :groups="groups" />
+      </ClientOnly>-->
+    </UDashboardLayout>
+</template>
