248a8fa3613979979478bb7c8402f7bdd139f37b
Bitti <Bitti09@users.noreply.github.com>
2024-03-10 23:18:13 +0100

    Refactor movie and TV pages

--- pages/tv.vue
+++ pages/tv.vue
@@ -1,18 +1,26 @@
 <template>
-    <UDashboardPage>
-        <UDashboardPanel grow>
-            <UDashboardNavbar title="Home">
-                <template #right>
-
-                </template>
-            </UDashboardNavbar>
-            {{ data }}
-        </UDashboardPanel>
-    </UDashboardPage>
+    <div>
+        {{ data }}
+        <NuxtImg provider="bunny" :src="data[0].poster" height="250" width="200" :quality="90" />
+        {{ grouped2 }}
+        {{ grouped }}
+    </div>
 </template>
 <script setup>
 const { getItems } = useDirectusItems();
+const grouped = ref([]);
+const grouped2 = ref([]);
 const data = await getItems({
     collection: "mediathek",
 })
+grouped.value = useKeys(
+    useCountBy(data, function (data) {
+        return data.country;
+    })
+);
+grouped2.value = useGroupBy(data, (item) => item.country);
+
+const { isDashboardSidebarSlidoverOpen } = useUIState()
+console.log(isDashboardSidebarSlidoverOpen.value)
+const title1 = isDashboardSidebarSlidoverOpen.value ? 'Mediathek Community - Home' : 'Home'
 </script>
