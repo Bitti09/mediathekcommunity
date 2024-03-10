2886ab4a50e44cae06258a09c5841c779f4dc083
Bitti <Bitti09@users.noreply.github.com>
2024-03-10 23:20:42 +0100

    Update Footer.vue with TMDB API disclaimer

--- components/Footer.vue
+++ components/Footer.vue
@@ -59,23 +59,30 @@ function onSubmit() {
 
     <template #left>
       <p class="text-gray-500 dark:text-gray-400 text-sm">
-        Copyright © {{ new Date().getFullYear() }}. All rights reserved.
+        Copyright © {{ new Date().getFullYear() }}. All rights reserved.<br>
+        This project uses the TMDB API but is not endorsed or certified by TMDB.
+
       </p>
     </template>
 
     <template #center>
-            <UColorModeButton size="sm" />
+      <UColorModeButton size="sm" />
     </template>
     <template #right>
 
       <div flex="~ row gap2" items-center>
-      <p text-true-gray:90 text-sm>
-        Data provided by&nbsp;
-        <a op100 target="_blank" href="https://www.themoviedb.org/" title="The Movie Database" rel="noopener" n-link-text>
-          <IconTMDB inline h4 />
-        </a>
-      </p>
-    </div>
+        <p text-true-gray:90 text-sm>
+          Data provided by&nbsp;
+          <a op100 target="_blank" href="https://www.themoviedb.org/" title="The Movie Database" rel="noopener"
+            n-link-text>
+            <IconTMDB inline h4 />
+          </a>
+        </p>
+      </div>
+      <div flex="~ row gap2" items-center>
+        <p text-true-gray:90 text-sm>
+        </p>
+      </div>
     </template>
   </UFooter>
 </template>
