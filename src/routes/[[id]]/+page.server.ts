cf5ba7549d05ddc1fbb2ffa9e9f64bfa92af2bbd
Bitti <Bitti09@users.noreply.github.com>
2024-07-03 14:22:38 +0200

    feat: Add logging for incoming request headers
    Included logging of incoming request headers on page load to assist with debugging and analysis of requests.

--- src/routes/[[id]]/+page.server.ts
+++ src/routes/[[id]]/+page.server.ts
@@ -1,12 +1,12 @@
 import { db } from '$lib/db/dbs';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({request }) => {
     const posts = await db.query.mediathek.findMany({
         with: {
             episodes: true
         }
     });
-
+    console.log(request.headers)
     return { posts };
 };
