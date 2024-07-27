d9cc533808fa84e6d82a4ecf07e7074f946ef210
Bitti <Bitti09@users.noreply.github.com>
2024-07-28 00:54:38 +0200

    Refactor: Streamline Directus SDK usage
    Removed unused import statements for several data manipulation functions (`updateItem`, `updateUser`, `createItem`, `deleteItem`) from the Directus SDK, optimizing the code for clarity and maintainability.

--- src/lib/directus.js
+++ src/lib/directus.js
@@ -1,11 +1,11 @@
 // @ts-nocheck
 import { createDirectus, rest } from '@directus/sdk';
-import { readItems, readItem, updateItem, updateUser, createItem, deleteItem } from '@directus/sdk';
+import { readItems, readItem } from '@directus/sdk';
 import { DIRECTUS_APIURL } from '$env/static/private';
 
 function getDirectusInstance(fetch) {
-  	const options = fetch ? { globals: { fetch } } : {};
-	const directus = createDirectus(DIRECTUS_APIURL, options ).with(rest());
+	const options = fetch ? { globals: { fetch } } : {};
+	const directus = createDirectus(DIRECTUS_APIURL, options).with(rest());
 	return directus;
 }
 
