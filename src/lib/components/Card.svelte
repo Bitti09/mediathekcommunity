c9c680101c3f2f1c74ab6b1f77ab67b88d1c222a
Bitti <Bitti09@users.noreply.github.com>
2024-07-28 01:20:46 +0200

    Fix: Corrected conditional rendering logic for unreleased cards.
    Ensured that cards with future publish dates are consistently hidden until their intended release date, regardless of the viewer's geographical location.

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -22,7 +22,7 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 {#if inFuture(carddata.publishdate) == false}
-<!--  dev only -->>
+<!--  dev only -->
 	{#if carddata.channel.country !== geo} 
 		<div class="flex">
 			<div class="relative mx-auto max-w-xl">
