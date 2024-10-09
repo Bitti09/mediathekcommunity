172df915d47ed7ed53ca8a2035ea099739d80b7a
bitti06 <Bitti09@users.noreply.github.com>
2024-10-09 20:30:07 +0000

    ..
    Signed-off-by: bitti06 <Bitti09@users.noreply.github.com>

--- src/lib/components/Videoplayer.svelte
+++ src/lib/components/Videoplayer.svelte
@@ -1,19 +1,20 @@
-<script>
+<script lang="ts">
 	import { onDestroy, onMount } from 'svelte';
 	import '../videojs/skins/treso/videojs.min.css';
 	import videojs from 'video.js';
 	import '../videojs/plugins/es/nuevo.js';
 	import '../videojs/plugins/es/playlist.js';
 	import '../videojs/plugins/es/videojs.hotkeys';
+
 	import { modalProps, modalvideo, omulist, noomulist, seriestype } from '$lib/store.js';
 
-	let player;
-	let videoElement;
+	let player: videojs.Player | null = null;
 
 	const videojsOptions = {
 		controls: true,
 		preload: 'auto',
 		playsinline: true,
+		fill: true,
 		hotkeys: true,
 		resume: true,
 		html5: {
@@ -36,18 +37,61 @@
 		pipButton: false,
 		buttonForward: true,
 		androidLock: true,
-		zoomMenu: false
+		zoomMenu: false,
+		theaterButton: false,
+		rateMenu: false,
+		settingsButton: true,
+		resume: true
 	};
 
-	onMount(() => {
+	function initializePlayer(videoElement: HTMLVideoElement) {
 		player = videojs(videoElement, videojsOptions);
 		player.nuevo(nuevoOptions);
+		player.hotkeys({ seekStep: 10 });
+		updatePlayerSource($seriestype);
 
-		return () => {
-			if (player) {
-				player.dispose();
+		player.on('mode', (event, mode) => {
+			const leftColumn = document.querySelector('#left_column');
+			if (leftColumn) {
+				leftColumn.style.width = mode === 'large' ? '100%' : '70%';
 			}
-		};
+		});
+	}
+
+	function updatePlayerSource(type: string) {
+		if (!player) return;
+
+		player.pause();
+		player.currentTime(0);
+
+		switch (type) {
+			case 'noomu':
+				player.playlist($noomulist);
+				break;
+			case 'omu':
+				player.playlist($omulist);
+				break;
+			default:
+				const videoSource = {
+					sources: [{ src: $modalvideo.src, type: $modalvideo.type }],
+					poster: $modalvideo.poster,
+					title: $modalvideo.title
+				};
+				player.poster($modalvideo.poster);
+				player.src(videoSource.sources);
+				break;
+		}
+	}
+
+	$: if (player && ($modalvideo || $seriestype)) {
+		updatePlayerSource($seriestype);
+	}
+
+	onMount(() => {
+		const videoElement = document.getElementById('my-video') as HTMLVideoElement;
+		if (videoElement) {
+			initializePlayer(videoElement);
+		}
 	});
 
 	onDestroy(() => {
@@ -57,10 +101,25 @@
 	});
 </script>
 
-<video
-	id="my-video"
-	playsinline 
-	webkit-playsinline 
-	class="video-js vjs-16-9 overflow-hidden"
-	bind:this={videoElement}
-></video>
+<div id="left_column" class="left-column">
+	<video 
+		id="my-video" 
+		playsinline 
+		webkit-playsinline 
+		class="video-js vjs-16-9 overflow-hidden"
+	></video>
+</div>
+
+<style>
+	.overflow-hidden {
+		overflow: hidden !important;
+	}
+	.left-column {
+		width: 100%;
+		display: flex;
+		height: fit-content;
+		max-height: fit-content;
+		align-items: center;
+		overflow: hidden;
+	}
+</style>
